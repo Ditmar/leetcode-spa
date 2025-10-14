ae4616b90d1854ca64c9901855f6d5ad26be3555
JoseHuanaco <huanacocamatajose@gmail.com>
2025-10-14 15:01:17 -0400

    Add: FormInput component

---
+++ src/components/FormInput/FormInput.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import type { FormInputProps } from './FormInput.types';
+import { StyledFormInput } from './FormInput.styles';
+import { DEFAULT_PROPS } from './FormInput.constants';
+
+export const FormInput: React.FC<FormInputProps> = ({
+  variant = DEFAULT_PROPS.variant,
+  type = DEFAULT_PROPS.type,
+  size = DEFAULT_PROPS.size,
+  placeholder,
+  value,
+  onChange,
+  errorMessage,
+  disabled = DEFAULT_PROPS.disabled,
+  onFocus,
+  onBlur,
+  'data-testid': dataTestId,
+  ...rest
+}) => {
+  const isError = !!errorMessage;
+
+  const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    if (onFocus) onFocus(event);
+  };
+
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    if (onBlur) onBlur(event);
+  };
+
+  return (
+    <StyledFormInput
+      variant={variant}
+      size={size}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      error={isError}
+      helperText={isError ? errorMessage : undefined}
+      disabled={disabled}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      data-testid={dataTestId}
+      {...rest}
+      inputProps={{ 'aria-label': placeholder || 'input' }}
+    />
+  );
+};
+
+export default FormInput;
+
