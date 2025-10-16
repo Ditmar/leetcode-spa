9ceff07cc276fa68c7d2ac49dd946c42c24f186f
vanesa quispe <quispevanesa401@gmail.com>
2025-10-16 16:42:46 -0400

    feat: add menu badge component

---
+++ src/components/Menu-Badge/MenuBadge.stories.tsx
@@ -0,0 +1,20 @@
+import { MenuBadge } from './MenuBadge';
+
+export default {
+  title: 'Components/MenuBadge',
+  component: MenuBadge,
+};
+
+export const Default = {
+  args: {
+    label: 'Dashboard',
+    variant: 'default',
+  },
+};
+
+export const Color = {
+  args: {
+    label: 'Dashboard',
+    variant: 'color',
+  },
+};
