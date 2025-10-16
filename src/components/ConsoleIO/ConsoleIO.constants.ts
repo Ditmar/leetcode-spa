3b2b355f847bbd660321253b2f03d8fddaa4e4d5
AlvaroC <albarocondori0123@gmail.com>
2025-10-16 11:05:23 -0400

    fix: update ConsoleIO with review suggestions

--- src/components/ConsoleIO/ConsoleIO.constants.ts
+++ src/components/ConsoleIO/ConsoleIO.constants.ts
@@ -1,9 +1,9 @@
 export const CONSOLE_DEFAULTS = {
-  WIDTH: 260,
-  HEIGHT: 502,
-  FRAME_MIN_HEIGHT: 460,
-  PADDING_PX: 20,
-  GAP_PX: 10,
+  WIDTH_SP: 32, // antes 260px => theme.spacing(32) = 256px aprox
+  HEIGHT_SP: 62, // antes 502px => theme.spacing(62) = 496px aprox
+  FRAME_MIN_HEIGHT_SP: 57, // antes 460px => theme.spacing(57) = 456px aprox
+  PADDING_SP: 2, // antes 20px => theme.spacing(2) = 16px aprox
+  GAP_SP: 1, // antes 10px => theme.spacing(1) = 8px aprox
   PLACEHOLDER_SAMPLE: 'Write Your Input Cases Here Like 1234',
   DEFAULT_CUSTOM_SAMPLE: `2
 6 3
