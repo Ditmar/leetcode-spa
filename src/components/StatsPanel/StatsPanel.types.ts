82f4639daccfe1b51e1ad7436091e1c530d03666
jacob santos <jeisantosayaviri@gmail.com>
2025-10-17 00:56:13 -0400

    [CDD] Build StatsPanel component

--- src/components/StatsPanel/StatsPanel.types.ts
+++ src/components/StatsPanel/StatsPanel.types.ts
@@ -1,16 +1,16 @@
 import type { ElementType } from 'react';
 
 export interface StatsPanelProps {
-    totalTests: number;
-    passed: number;
-    failed: number;
-    waiting: number;
+  totalTests: number;
+  passed: number;
+  failed: number;
+  waiting: number;
 }
 
 export interface StatItemData {
-    id: string;
-    label: string;
-    value: number;
-    icon: ElementType;
-    color: string;
+  id: string;
+  label: string;
+  value: number;
+  icon: ElementType;
+  color: string;
 }
