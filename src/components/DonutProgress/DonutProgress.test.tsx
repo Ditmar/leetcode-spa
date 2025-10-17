0f63c34ef2cab9581b7cab09536cdccdf8b56cf5
Crisstmau <mauricioblasquez003@gmail.com>
2025-10-17 09:21:12 -0400

    add component DonutProgress fix lint

--- src/components/DonutProgress/DonutProgress.test.tsx
+++ src/components/DonutProgress/DonutProgress.test.tsx
@@ -1,11 +1,12 @@
-import { render, screen } from "@testing-library/react";
-import { DonutProgress } from "./DonutProgress";
-import { describe, expect, it } from "vitest";
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import { DonutProgress } from './DonutProgress';
 import '@testing-library/jest-dom';
 
-describe("DonutProgress", () => {
-  it("renderizar", () => {
+describe('DonutProgress', () => {
+  it('renderizar', () => {
     render(<DonutProgress percentage={50} />);
-    expect(screen.getByText("50%")).toBeInTheDocument();
+    expect(screen.getByText('50%')).toBeInTheDocument();
   });
 });
