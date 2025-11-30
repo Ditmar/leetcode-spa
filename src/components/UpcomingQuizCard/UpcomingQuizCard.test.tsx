7a0a2b37d2b3e828c9652ffb5d8cf1cf6920df73
kellyalejandra861 <gutierrezmachacakellyalejandra@gmail.com>
2025-11-29 21:57:10 -0400

    feature(fix): fix the design of the ticket

--- src/components/UpcomingQuizCard/UpcomingQuizCard.test.tsx
+++ src/components/UpcomingQuizCard/UpcomingQuizCard.test.tsx
@@ -1,13 +1,28 @@
+import { ThemeProvider } from '@mui/material/styles';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, it, expect, vi } from 'vitest';
+
+import theme from '../../style-library/theme/theme';
 
 import { UpcomingQuizCard } from './UpcomingQuizCard';
 
 describe('UpcomingQuizCard', () => {
   it('calls onRegister when button is clicked', () => {
     const handleRegister = vi.fn();
-    render(<UpcomingQuizCard title="Quiz" date="2023-08-12" onRegister={handleRegister} />);
+
+    render(
+      <ThemeProvider theme={theme}>
+        <UpcomingQuizCard
+          title="Quiz"
+          date="2023-08-12"
+          description="Test description"
+          onRegister={handleRegister}
+        />
+      </ThemeProvider>
+    );
+
     fireEvent.click(screen.getByTestId('register-button'));
+
     expect(handleRegister).toHaveBeenCalledTimes(1);
   });
 });
