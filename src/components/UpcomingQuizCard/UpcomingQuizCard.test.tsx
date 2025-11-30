1fb1f56ef58c927153f267a89af51f522c5f855e
kellyalejandra861 <gutierrezmachacakellyalejandra@gmail.com>
2025-11-29 22:13:48 -0400

    feature(fix): fix the design of the ticket

--- src/components/UpcomingQuizCard/UpcomingQuizCard.test.tsx
+++ src/components/UpcomingQuizCard/UpcomingQuizCard.test.tsx
@@ -1,63 +1,29 @@
-import { ThemeProvider } from '@mui/material/styles';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, it, expect, vi } from 'vitest';
-
-import theme from '../../style-library/theme/theme';
 
 import { UpcomingQuizCard } from './UpcomingQuizCard';
 
 describe('UpcomingQuizCard', () => {
-  it('renders the title correctly', () => {
-    render(
-      <ThemeProvider theme={theme}>
-        <UpcomingQuizCard title="Sample Quiz" date="2023-08-12" onRegister={vi.fn()} />
-      </ThemeProvider>
-    );
+  it('renders the title', () => {
+    render(<UpcomingQuizCard title="Sample Quiz" date="2023-08-12" onRegister={vi.fn()} />);
     expect(screen.getByText('Sample Quiz')).toBeInTheDocument();
   });
 
   it('renders the description if provided', () => {
     render(
-      <ThemeProvider theme={theme}>
-        <UpcomingQuizCard
-          title="Sample Quiz"
-          date="2023-08-12"
-          description="This is a test description"
-          onRegister={vi.fn()}
-        />
-      </ThemeProvider>
+      <UpcomingQuizCard
+        title="Sample Quiz"
+        date="2023-08-12"
+        description="This is a test description"
+        onRegister={vi.fn()}
+      />
     );
     expect(screen.getByText('This is a test description')).toBeInTheDocument();
   });
 
-  it('does not render description if not provided', () => {
-    render(
-      <ThemeProvider theme={theme}>
-        <UpcomingQuizCard title="Sample Quiz" date="2023-08-12" onRegister={vi.fn()} />
-      </ThemeProvider>
-    );
-    const desc = screen.queryByText('This is a test description');
-    expect(desc).not.toBeInTheDocument();
-  });
-
-  it('renders the calendar icon', () => {
-    render(
-      <ThemeProvider theme={theme}>
-        <UpcomingQuizCard title="Sample Quiz" date="2023-08-12" onRegister={vi.fn()} />
-      </ThemeProvider>
-    );
-    const img = screen.getByAltText('Calendar Icon') as HTMLImageElement;
-    expect(img).toBeInTheDocument();
-    expect(img.src).toContain('Calendar.png'); // ajusta el nombre según tu import
-  });
-
   it('calls onRegister when register button is clicked', () => {
     const handleRegister = vi.fn();
-    render(
-      <ThemeProvider theme={theme}>
-        <UpcomingQuizCard title="Sample Quiz" date="2023-08-12" onRegister={handleRegister} />
-      </ThemeProvider>
-    );
+    render(<UpcomingQuizCard title="Sample Quiz" date="2023-08-12" onRegister={handleRegister} />);
     fireEvent.click(screen.getByTestId('register-button'));
     expect(handleRegister).toHaveBeenCalledTimes(1);
   });
