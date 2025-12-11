06ceb6b6a1a0bd9603cdde8a61061e73cc67be25
Yovanaedith <yovanaedith2000@gmail.com>
2025-12-11 15:59:44 -0200

    Refactor: Applied strict email regular expression to LoginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -108,7 +108,8 @@ const PrimaryButton: React.FC<PrimaryButtonFallbackProps> = (props) => (
   </Button>
 );
 
-const EMAIL_REGEX = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const EMAIL_REGEX =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const PASSWORD_MIN_LENGTH = 6;
 
 export const LoginPage: React.FC<LoginPageProps> = ({
