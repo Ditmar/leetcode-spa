ff3f4a74eb8e967ffea7cf9b80f120915b0df6f8
jacob santos <jacobsantos4590@gmail.com>
2025-12-14 19:00:42 -0400

    rebase2

---
+++ src/components/StatsPanel/StatsPanel.types.ts
@@ -0,0 +1,18 @@
+import type { ElementType } from 'react';
+
+export interface StatsPanelProps {
+  totalTests: number;
+  passed: number;
+  failed: number;
+  waiting: number;
+}
+
+export type StatColor = 'primary' | 'secondary' | 'error' | 'warning' | 'success' | 'info' | 'text';
+
+export interface StatItemData {
+  id: string;
+  label: string;
+  value: number;
+  icon: string | ElementType;
+  color: StatColor;
+}
