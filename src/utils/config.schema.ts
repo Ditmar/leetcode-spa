e6fb54cf6af5ae5ba2cf9cff05884ab3049c4710
JavierPintoC <javier123gabriel95@gmail.com>
2025-12-15 08:42:58 -0400

    Rebase

--- src/utils/config.schema.ts
+++ src/utils/config.schema.ts
@@ -1,9 +1,8 @@
 import { z } from 'zod';
 
-// Definimos las reglas: debe ser un número positivo
+// We define the rules: it must be a positive number
 export const configSchema = z.object({
   defaultCacheTTL: z.number().positive().describe('Cache TTL in milliseconds'),
 });
-
-// Extraemos el tipo de TypeScript automáticamente de Zod
+// We automatically extract the TypeScript type from Zod
 export type Config = z.infer<typeof configSchema>;
