04e50d115c0850318054ce088d285634e20b3451
Hersom Canaza <hersoncanazadelgado@gmail.com>
2026-04-03 00:42:28 -0400

    feat: create CDD compoment Collapsible using the code documentation

---
+++ src/component-catalog/Collapsible/Collapsible.styles.ts
@@ -0,0 +1,30 @@
+import { styled } from '@mui/material/styles';
+import { Box, Stack, Button, Typography } from '@mui/material';
+
+export const StyledCard = styled(Box)(({ theme }) => ({
+  borderRadius: '14px', 
+  backgroundColor: theme.palette.common.white,
+  border: `1.2px solid ${theme.palette.divider}`,
+  padding: theme.spacing(3),
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  maxWidth: '400px',
+  boxSizing: 'border-box',
+}));
+
+export const Header = styled(Stack)(({ theme }) => ({
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  minHeight: theme.spacing(5.5), 
+}));
+
+export const BlackButton = styled(Button)(({ theme }) => ({
+  background: '#030213', 
+  color: '#fff',
+  minWidth: theme.spacing(4.5),
+  height: theme.spacing(4),
+  borderRadius: '8px',
+  '&:hover': { background: '#030213', opacity: 0.9 },
+}));
