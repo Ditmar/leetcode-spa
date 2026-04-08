4c6c5724e1932b0169b3dc9667646b24b066d60c
Fernanda Quinteros <quinterosf246@gmail.com>
2026-04-07 23:40:34 -0400

    feat: create CDD component DropdownMenu using the code documentation

--- src/component-catalog/DropdownMenu/DropdownMenu.constants.ts
+++ src/component-catalog/DropdownMenu/DropdownMenu.constants.ts
@@ -9,14 +9,12 @@ export const DROPDOWN_ITEM_TYPES = {
 export type DropdownItemType = (typeof DROPDOWN_ITEM_TYPES)[keyof typeof DROPDOWN_ITEM_TYPES];
 export const DEFAULT_GROUP_ID = 'default-group';
 
-
 export const ARIA_ROLES = {
   MENU: 'menu',
   MENU_ITEM: 'menuitem',
   MENU_ITEM_CHECKBOX: 'menuitemcheckbox',
   MENU_ITEM_RADIO: 'menuitemradio',
 } as const;
-
 
 export const DROPDOWN_DEFAULTS = {
   MOBILE_MIN_HEIGHT: 44,
