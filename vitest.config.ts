ff0e8838837eb807fe65f2e6e7194ea8d713f705
J.Llanos <92152054+JesusAlexanderLlanosLlanos@users.noreply.github.com>
2026-04-13 09:59:31 -0400

    fix(test): switch to happy-dom to fix CI jsdom error

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'happy-dom',
     setupFiles: ['./src/test/setup.ts'],
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: ['src/**/*.stories.{js,ts,jsx,tsx}', 'node_modules/**'],
