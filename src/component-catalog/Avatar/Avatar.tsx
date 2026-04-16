938e4dac39f0ddfc44aef6cb97e4b2edf2e43b6f
ErikaNDelgadillo450 <erikadelgadilloquispe@gmail.com>
2026-04-15 22:54:35 -0400

    Avatar: quinto comit

--- src/component-catalog/Avatar/Avatar.tsx
+++ src/component-catalog/Avatar/Avatar.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 
 import { StyledAvatar } from './Avatar.styles';
+
 import type { AvatarProps } from './Avatar.types';
 
-const Avatar = ({
-  children,
-  size = 'md',
-  variant = 'default',
-  ...props
-}: AvatarProps) => {
+const Avatar = ({ children, size = 'md', variant = 'default', ...props }: AvatarProps) => {
   return (
     <StyledAvatar
       $size={size}
