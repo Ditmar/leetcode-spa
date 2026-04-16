9dfd91ddc1ac5710330c8a6aff7da551138ca629
ALE.CV <paola@example.com>
2026-04-16 04:15:10 -0400

    fix: correct SSRMuiProvider export and css issue

--- src/style-library/SSRMuiProvider.tsx
+++ src/style-library/SSRMuiProvider.tsx
@@ -1,11 +1,32 @@
-import MuiThemeProvider from './provider/MuiThemeProvider';
+import { CacheProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import { useState, useEffect, type ReactNode } from 'react';
 
-import type { ReactNode } from 'react';
+import { theme } from '../theme';
+import { createEmotionCache } from '../utils/createEmotionCache';
 
 interface SSRMuiProviderProps {
   children: ReactNode;
 }
 
-export default function SSRMuiProvider({ children }: SSRMuiProviderProps) {
-  return <MuiThemeProvider>{children}</MuiThemeProvider>;
-}
+export const SSRMuiProvider = ({ children }: SSRMuiProviderProps) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  const cache = createEmotionCache();
+
+  return (
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
