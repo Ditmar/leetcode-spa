6a897517175b91789fd95e20ee3f638c7ef8dde3
ErikaNDelgadillo450 <erikadelgadilloquispe@gmail.com>
2026-04-15 22:48:09 -0400

    Avtar: cuarto comit

--- src/component-catalog/Avatar/Avatar.tsx
+++ src/component-catalog/Avatar/Avatar.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 import { StyledAvatar } from './Avatar.styles';
-import type  { AvatarProps } from './Avatar.types';
+import type { AvatarProps } from './Avatar.types';
 
-/**
- * Avatar component for displaying user initials or profile representations.
- */
-const Avatar = ({ children, size = 'md', variant = 'default', ...props }: AvatarProps) => {
+const Avatar = ({
+  children,
+  size = 'md',
+  variant = 'default',
+  ...props
+}: AvatarProps) => {
   return (
     <StyledAvatar
       $size={size}
       $variant={variant}
       data-testid="avatar-component"
-      // Pasamos los valores como data-attributes para que el test pueda leerlos
-      // sin depender del motor de renderizado de CSS de jsdom
       data-size={size}
       data-variant={variant}
       {...props}
