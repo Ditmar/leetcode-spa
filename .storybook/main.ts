25fc57494f2462633de102f71b92aa6a9ac465a6
ditmarcastro@gmail.com <ditmarcastro@gmail.com>
2026-04-21 15:39:26 -0400

    feat: update ci

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -11,6 +12,11 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+  viteFinal(config) {
+    return mergeConfig(config, {
+      base: process.env.STORYBOOK_BASE_URL ?? '/',
+    });
   },
 };
 export default config;
