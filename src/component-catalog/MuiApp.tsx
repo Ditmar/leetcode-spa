643687df5728f19e2af11cae2a61338cb418efb8
ErickTorrez <gabo1cnc@gmail.com>
2026-04-23 00:31:58 -0400

    feat: update VSCode settings and fix TypeScript configuration for JSX

--- src/component-catalog/MuiApp.tsx
+++ src/component-catalog/MuiApp.tsx
@@ -7,6 +7,7 @@ import SimpleMuiExample from '../style-library/stories/SimpleMuiExample';
 
 import type { EmotionCache } from '@emotion/react';
 
+// Crear un tema básico
 const theme = createTheme({
   palette: {
     mode: 'light',
