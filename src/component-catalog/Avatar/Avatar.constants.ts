1c099ec21fd287a2e5fa2d847cf8cb5b8f7c249f
ErikaNDelgadillo450 <erikadelgadilloquispe@gmail.com>
2026-04-25 21:51:44 -0400

    Avatar: sexto commit

--- src/component-catalog/Avatar/Avatar.constants.ts
+++ src/component-catalog/Avatar/Avatar.constants.ts
@@ -1,26 +1,12 @@
-export type AvatarVariantType = 'sm' | 'md' | 'lg' | 'xl';
+export type AvatarSizeType = 'sm' | 'md' | 'lg' | 'xl';
 export type AvatarColorType = 'default' | 'primary' | 'success';
 
 export const AVATAR_SIZES: Record<
-  AvatarVariantType,
+  AvatarSizeType,
   { width: number; height: number; fontSize: number }
 > = {
-  sm: { width: 32, height: 32, fontSize: 12 },
-  md: { width: 40, height: 40, fontSize: 14 },
-  lg: { width: 48, height: 48, fontSize: 16 },
-  xl: { width: 64, height: 64, fontSize: 20 },
-};
-export const AVATAR_VARIANTS: Record<AvatarColorType, { bg: string; color: string }> = {
-  default: {
-    bg: 'var(--avatar-default-bg)',
-    color: 'var(--avatar-default-text)',
-  },
-  primary: {
-    bg: 'var(--avatar-primary-bg)',
-    color: 'var(--avatar-text-light)',
-  },
-  success: {
-    bg: 'var(--avatar-success-bg)',
-    color: 'var(--avatar-text-light)',
-  },
+  sm: { width: 32, height: 32, fontSize: 11 },
+  md: { width: 40, height: 40, fontSize: 13 },
+  lg: { width: 48, height: 48, fontSize: 14 },
+  xl: { width: 64, height: 64, fontSize: 16 },
 };
