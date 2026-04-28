20dec757422e8178fef94f741ee92d6b061f5f93
Jefferson Tito Gumiel <gumieljefferson@gmail.com>
2026-04-27 23:42:39 -0400

    ordering imports in HoverCard.stories

--- src/component-catalog/hovercard/HoverCard.stories.tsx
+++ src/component-catalog/hovercard/HoverCard.stories.tsx
@@ -5,6 +5,7 @@ import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 
 import { HoverCard } from './HoverCard';
+
 import {
   DemoPageRoot,
   DemoPageTitle,
