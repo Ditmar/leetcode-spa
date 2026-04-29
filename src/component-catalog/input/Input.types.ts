0b3b13668164a62be7b499f09a8b86d112ad17fd
BelenQuinteros68 <163065341+BelenQuinteros68@users.noreply.github.com>
2026-04-28 21:34:29 -0400

    feat: create CDD component Input using the code documentation (#212)
    * feat: create CDD component Input using the code documentation
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * [O:
    feat: create CDD component Input
    
    * feat: create CDD component Input using the code documentation
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input
    
    * feat: create CDD component Input

---
+++ src/component-catalog/input/Input.types.ts
@@ -0,0 +1,16 @@
+import type { InputTypes, InputVariants } from './Input.constants';
+import type { TextFieldProps } from '@mui/material';
+import type { ReactNode } from 'react';
+
+export interface InputProps extends Omit<TextFieldProps, 'type' | 'variant'> {
+  type?: InputTypes;
+  variant?: InputVariants;
+  startAdornment?: ReactNode;
+  endAdornment?: ReactNode;
+  /**
+   * Shortcut for inputProps.maxLength
+   */
+  maxLength?: number;
+  showCharacterCount?: boolean;
+  readOnly?: boolean;
+}
