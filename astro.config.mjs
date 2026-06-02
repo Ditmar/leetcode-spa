daee3b93a0e5f86afb003bcb2f371bba1de4bc37
Ditmar <spyatorio@gmail.com>
2026-06-02 16:30:46 -0400

    feat: enhace pipelines and added dockerfile build an publish (#283)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,21 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import dotenv from 'dotenv';
+
+if (process.env.ENV !== 'production') {
+  dotenv.config();
+}
 
 export default defineConfig({
+  output: 'static',
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT)|| 4321,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 4321,
+  },
   integrations: [
     react({
       experimentalReactChildren: true,
