4007af1ff356bc8d9e357ff3bfb277bb5d3da395
ditmarcastro@gmail.com <ditmarcastro@gmail.com>
2026-06-03 18:13:15 -0400

    feat: update host

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,12 @@ export default defineConfig({
   output: 'static',
   server: {
     host: '0.0.0.0',
-    port: Number(process.env.PORT)|| 4321,
+    port: Number(process.env.PORT) || 4321,
+  },
+  vite: {
+    preview: {
+      allowedHosts: true,
+    },
   },
   preview: {
     host: '0.0.0.0',
