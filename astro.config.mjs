f04e2762929e789fbebeff6c7a1be097710733da
ditmarcastro@gmail.com <ditmarcastro@gmail.com>
2026-06-03 22:49:04 -0400

    feat: fix allowed host

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,14 +12,10 @@ export default defineConfig({
     host: '0.0.0.0',
     port: Number(process.env.PORT) || 4321,
   },
-  vite: {
-    preview: {
-      allowedHosts: true,
-    },
-  },
   preview: {
     host: '0.0.0.0',
     port: Number(process.env.PORT) || 4321,
+    allowedHosts: true,
   },
   integrations: [
     react({
