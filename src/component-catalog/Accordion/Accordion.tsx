57b2c341fc463226189a88b1c824bbf4ad038221
JhonnMDX82 <leonmdx82@gmail.com>
2026-07-01 17:42:43 -0400

    fix: resolved comments

--- src/component-catalog/Accordion/Accordion.tsx
+++ src/component-catalog/Accordion/Accordion.tsx
@@ -16,6 +16,21 @@ export const Accordion = (props: AccordionProps) => {
   const { itemsWithState } = useAccordionState(props);
   const { disableAnimation, square, slotProps, expandIcon } = props;
 
+  const getValidSlotProps = () => {
+    const accordionProps = { ...slotProps?.accordion };
+    const summaryProps = { ...slotProps?.summary };
+    const detailsProps = { ...slotProps?.details };
+
+    delete accordionProps.expanded;
+    delete accordionProps.onChange;
+    delete accordionProps.disabled;
+    delete summaryProps.expandIcon;
+
+    return { accordionProps, summaryProps, detailsProps };
+  };
+
+  const validSlotProps = getValidSlotProps();
+
   return (
     <AccordionContainer square={square} data-testid="accordion-container">
       {itemsWithState.map((item) => {
@@ -27,21 +42,21 @@ export const Accordion = (props: AccordionProps) => {
             disableGutters
             square={square}
             TransitionProps={{ timeout: disableAnimation ? 0 : undefined }}
-            {...slotProps?.accordion}
+            {...validSlotProps.accordionProps}
             expanded={item.expanded}
             onChange={item.onChange}
             disabled={item.disabled}
             data-testid={`accordion-${item.id}`}
           >
             <StyledAccordionSummary
-              {...slotProps?.summary}
+              {...validSlotProps.summaryProps}
               expandIcon={<TouchTarget>{summaryIcon}</TouchTarget>}
               data-testid={`accordion-summary-${item.id}`}
             >
               {item.summary}
             </StyledAccordionSummary>
             <StyledAccordionDetails
-              {...slotProps?.details}
+              {...validSlotProps.detailsProps}
               data-testid={`accordion-details-${item.id}`}
             >
               {item.details}
